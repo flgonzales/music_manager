9fb628a9d4dfcc628f273261be47d86acf7cf817
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-06-14 00:19:03 +0200

    Debug of Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Artist.create {(name:"Pata de Elefante", avatar:"https://i.ytimg.com/vi/zk45oKmi8EQ/hqdefault.jpg")}
-Artist.create {(name:"Laranja Freak", avatar:"https://i.ytimg.com/vi/cYvhNWcvfmg/maxresdefault.jpg")}
-Artist.create {(name:"Os Mutantes", avatar:"http://rollingstone.uol.com.br/media/images/original/2014/07/04/img-1024319-os-mutantes.jpg")}
-Artist.create {(name:"Gilberto Gil", avatar:"http://img.photobucket.com/albums/v627/Flabbergast/1.png")}
-Artist.create{(name:"Tim Maia", avatar:"http://www.luakabop.com/photobio/tim-maia/nobody-can-live-forever-tim-maia.jpg")}
+Artist.create(name:"Pata de Elefante", avatar:"https://i.ytimg.com/vi/zk45oKmi8EQ/hqdefault.jpg")
+Artist.create(name:"Laranja Freak", avatar:"https://i.ytimg.com/vi/cYvhNWcvfmg/maxresdefault.jpg")
+Artist.create(name:"Os Mutantes", avatar:"http://rollingstone.uol.com.br/media/images/original/2014/07/04/img-1024319-os-mutantes.jpg")
+Artist.create(name:"Gilberto Gil", avatar:"http://img.photobucket.com/albums/v627/Flabbergast/1.png")
+Artist.create(name:"Tim Maia", avatar:"http://www.luakabop.com/photobio/tim-maia/nobody-can-live-forever-tim-maia.jpg")
